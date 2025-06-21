e59d27f05cf34a359df767e09d3f54f89c0935e6
Wanda Martins <146781509+WandaMartins@users.noreply.github.com>
2025-06-21 10:48:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+  document.getElementById("mensagem").innerText = "Você é mais forte do que imagina. Continue!";
+}
+
+document.getElementById("botaoMensagem").onclick = mostrarMensagem;
